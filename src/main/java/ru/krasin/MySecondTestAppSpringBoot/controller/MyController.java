c33bc3d7603b73a48b3a58a489bf03a0159c65de
KrasinAnton <2saper@mail.ru>
2023-09-29 19:21:54 +0500

    Course_2_lr_2

---
+++ src/main/java/ru/krasin/MySecondTestAppSpringBoot/controller/MyController.java
@@ -0,0 +1,60 @@
+package ru.krasin.MySecondTestAppSpringBoot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.krasin.MySecondTestAppSpringBoot.exception.ValidationFailedException;
+import ru.krasin.MySecondTestAppSpringBoot.model.Request;
+import ru.krasin.MySecondTestAppSpringBoot.model.Response;
+import ru.krasin.MySecondTestAppSpringBoot.service.ValidationService;
+
+import javax.validation.Valid;
+import javax.validation.Validation;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RestController
+public class MyController {
+
+    private final ValidationService validationService;
+
+    @Autowired
+    public MyController(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
+    @PostMapping(value = "/feedback")
+    public ResponseEntity<Response> feedback(@Valid @RequestBody Request request,
+                                             BindingResult bindingResult) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+
+        Response response = Response.builder()
+                .uid(request.getUid())
+                .operationUid(request.getOperationUid())
+                .systemTime(simpleDateFormat.format(new Date()))
+                .code("success")
+                .errorCode("")
+                .errorMessage("")
+                .build();
+
+        try {
+            validationService.isValid(bindingResult);
+        } catch (ValidationFailedException e) {
+            response.setCode("failed");
+            response.setErrorCode("ValidationException");
+            response.setErrorMessage("Ошибка валидации");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }catch (Exception e) {
+            response.setCode("failed");
+            response.setErrorCode("UnknownException");
+            response.setErrorMessage("Произошла непредвиденная ошибка");
+            return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
